b7e24a11c7395ad157464dd735e5e7be6bf6ed69
Iomegan <dwitt.kiel@icloud.com>
2023-11-14 20:46:04 +0100

    Initial Commit

---
+++ Persist Adaptive Text Color/Persist_Adaptive_Text_ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Persist_Adaptive_Text_ColorApp.swift
+//  Persist Adaptive Text Color
+//
+//  Created by Daniel Witt on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Persist_Adaptive_Text_ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
